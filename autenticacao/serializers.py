de8a40bebed990675c5ae5d0a0d4bc88e06a1219
Higor Gustavo <higor-gustavo_lp@hotmail.com>
2025-03-06 01:05:17 -0300

    Rotas de refresh e logout

--- autenticacao/serializers.py
+++ autenticacao/serializers.py
@@ -1,4 +1,9 @@
-from rest_framework_simplejwt.serializers import TokenObtainPairSerializer
+from rest_framework import serializers
+from rest_framework_simplejwt.serializers import (
+    TokenBlacklistSerializer,
+    TokenObtainPairSerializer,
+    TokenRefreshSerializer,
+)
 
 
 class CustomTokenObtainPairSerializer(TokenObtainPairSerializer):
@@ -7,3 +12,24 @@ class CustomTokenObtainPairSerializer(TokenObtainPairSerializer):
         data["token"] = data.pop("access")
         data["refresh_token"] = data.pop("refresh")
         return data
+
+
+class CustomTokenRefreshSerializer(TokenRefreshSerializer):
+    refresh_token = serializers.CharField(required=True)
+    refresh = None
+
+    def validate(self, attrs):
+        attrs["refresh"] = attrs.pop("refresh_token")
+        data = super().validate(attrs)
+        data["token"] = data.pop("access")
+        data["refresh_token"] = data.pop("refresh")
+        return data
+
+
+class CustomTokenBlacklistSerializer(TokenBlacklistSerializer):
+    refresh_token = serializers.CharField(required=True)
+    refresh = None
+
+    def validate(self, attrs):
+        attrs["refresh"] = attrs.pop("refresh_token")
+        return super().validate(attrs)
--- autenticacao/urls.py
+++ autenticacao/urls.py
@@ -1,7 +1,11 @@
 from django.urls import path
-from rest_framework_simplejwt.views import TokenObtainPairView
+from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
+
+from .views import CustomTokenBlacklistView
 
 app_name = "autenticacao"
 urlpatterns = [
     path("login", TokenObtainPairView.as_view(), name="login"),
+    path("refresh", TokenRefreshView.as_view(), name="refresh"),
+    path("logout", CustomTokenBlacklistView.as_view(), name="logout"),
 ]
